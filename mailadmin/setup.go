32580e6dcd895a91914b9da92bfdf3789c1c00e0
Pedro Morgan <pedromorgan@gmail.com>
2015-09-12 09:53:36 +0100

    fix queries on mailboxes

--- mailadmin/setup.go
+++ mailadmin/setup.go
@@ -13,9 +13,13 @@ var Db *sqlx.DB
 
 func Setup( db *sqlx.DB, router *mux.Router){
 	Db = db
+
 	router.HandleFunc("/ajax/domains", DomainsAjaxHandler)
 	router.HandleFunc("/ajax/domain/{domain}", DomainAjaxHandler)
 
+	router.HandleFunc("/ajax/domain/{domain}/mailboxes", MailboxesAjaxHandler)
 
+	router.HandleFunc("/ajax/domain/{domain}/mailbox/{username}", MailboxAjaxHandler)
+	router.HandleFunc("/ajax/mailbox/{username}", MailboxAjaxHandler)
 
 }
