1f3d15ff0c3574e09fb3a919ed8fe08513409458
Pedro Morgan <pedromorgan@gmail.com>
2015-09-27 23:26:08 +0100

    rename all handlers and make more comments

--- postfixadmin/doc.go
+++ postfixadmin/doc.go
@@ -2,6 +2,8 @@
 // Administration of a postfixadmin database with ajax/rest
 package postfixadmin
 
+// Api Docs: http://godoc.org/github.com/daffodil/go-mailkit/postfixadmin
+//
 // The database is expected to have been created by postfixadmin https://github.com/postfixadmin/postfixadmin
 //
 // TODO docs
